20ece1613802a61f73e359898c9231612ab9b4a9
Ahmed Af <130662643+oCHRONOSo@users.noreply.github.com>
2026-05-10 22:11:53 +0200

    Change base path in Vite configuration

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Github_Practica/',
+  base: '/About_me_3D/',
   plugins: [react()],
   server: {
     host: '0.0.0.0', // Bind to all available network interfaces
